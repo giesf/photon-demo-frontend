dcd22169b44d6c611fc04b1893138eaa28bfd299
giesf <64958447+giesf@users.noreply.github.com>
2025-06-04 22:21:24 +0200

    Added timer

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function load(
     sp.append("layer","street")
     if(timeout){ clearTimeout(timeout)}
     timeout = setTimeout(()=>{
+        const start = new Date();
         fetch("https://photon.komoot.io/api?"+sp.toString(), {
             method: 'get',
             signal: controller.signal,
@@ -19,7 +20,9 @@ function load(
             const rr = r.features.map(r=>r.properties)
     
             const rstr = Array.from( new Set(rr.map(l=>`<li data-original='${JSON.stringify(l)}'>${l.street && l.name ?  l.name+", ":""}${l.street ?? l.name ?? ""}${l.housenumber ?" "+l.housenumber: ""}, ${l.district ? l.district+", ":""}${l.postcode} ${l.city}, ${l.country}</li>`)))
-            
+            const end = new Date();
+            const timeS = (end.valueOf()- start.valueOf() ) / 1000
+            document.getElementById("time").innerHTML="Query Time: "+timeS.toFixed(3) + "sec"
             document.getElementById("results").innerHTML=rstr.join("\n")
         })  )
     },200)
